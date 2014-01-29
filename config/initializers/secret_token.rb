4b2c670d83795c72a86676091fa94a0ba3fd34db
Nicki <nkeszler10@gmail.com>
2014-01-29 12:03:43 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giraffle::Application.config.secret_key_base = '28d43cb8ccf5d25703a6c505dbc8fcd54ac8fbd91c59f463887c1db7977d5d816029fba377768da36cac03b180af19c227623e25fc635f9ccc22f9fa07eb8dda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Giraffle::Application.config.session_store :cookie_store, key: '_giraffle_session'
